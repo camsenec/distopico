28515704449567d8059c22feb5319060476662a6
Tomoya Tanaka <deepsky2221@gmail.com>
2021-07-03 22:12:40 +0300

    Add model for general message

--- model/message.go
+++ model/message.go
@@ -2,12 +2,14 @@ package model
 
 import (
 	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type Message struct {
-	ID            uint64 `gorm:"AUTO_INCREMENT;primary_key;index"`
-	ApplicationID uint64
-	Message       string `gorm:"type:text"`
-	Topic         string `gorm:"type:text"`
-	Date          time.Time
+type MessageModel struct {
+	ID            primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	ApplicationID uint64             `bson:"applicationId,omitempty" json:"applicationId,omitempty"`
+	Description   string             `bson:"description" json:"description,omitempty"`
+	Topic         string             `bson:"topic" json:"topic,omitempty"`
+	CreatedData   time.Time          `bson:"createdDate,omitempty" json:"createdDate,omitempty"`
 }
