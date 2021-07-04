dacae61e8006d194da9450174677156f3722edfb
Tomoya Tanaka <deepsky2221@gmail.com>
2021-07-04 12:37:52 +0300

    add main routine

--- app.go
+++ app.go
@@ -1,9 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/distopico/database"
 	"github.com/distopico/router"
 )
 
 func main() {
-	router.Create()
+	db := database.ConnectDB()
+	defer db.CloseDB()
+
+	engine := router.Create(&db)
+
+	httpHandler := engine
+
+	addr := fmt.Sprintf("%s:%d", "127.0.0.1", 9000)
+	fmt.Println("Started Listening for plain HTTP connection on " + addr)
+	server := &http.Server{Addr: addr, Handler: httpHandler}
+
+	log.Fatal(server.ListenAndServe())
+
 }
