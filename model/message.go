2b33d7b531557c462baca3216965c65288819e1a
Tomoya Tanaka <deepsky2221@gmail.com>
2021-07-04 12:30:16 +0300

    Refactor message model

--- model/message.go
+++ model/message.go
@@ -7,7 +7,7 @@ import (
 )
 
 type MessageModel struct {
-	ID          primitive.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
+	ID          primitive.ObjectID `bson:"_id" json:"_id"`
 	Topic       string             `bson:"topic" json:"topic,omitempty"`
 	Description string             `bson:"description" json:"description,omitempty"`
 	CreatedDate time.Time          `bson:"createdDate,omitempty" json:"createdDate,omitempty"`
