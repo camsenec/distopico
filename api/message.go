99f4460b672751287f65fe8f873c700e4534617e
Tomoya Tanaka <deepsky2221@gmail.com>
2021-07-04 12:36:37 +0300

    Add api for messages

--- api/message.go
+++ api/message.go
@@ -1,7 +1,40 @@
 package api
 
 import (
-	"time"
+	"errors"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var timeNow = time.Now
+type MessageDatabase interface {
+	GetAllMessages() (interface{}, error)
+	GetMessageById(id primitive.ObjectID) (interface{}, error)
+}
+
+type MessageAPI struct {
+	DB MessageDatabase
+}
+
+func (a *MessageAPI) GetMessages(ctx *gin.Context) {
+	res, err := a.DB.GetAllMessages()
+	if err != nil {
+		ctx.AbortWithError(400, errors.New("Object not found"))
+	}
+	ctx.JSON(200, res)
+
+}
+
+func (a *MessageAPI) GetMessageByID(ctx *gin.Context) {
+	id, err := primitive.ObjectIDFromHex(ctx.Param("id"))
+	if err != nil {
+		ctx.AbortWithError(400, errors.New("Invalid id"))
+	}
+	fmt.Println(id)
+	res, err := a.DB.GetMessageById(id)
+	if err != nil {
+		ctx.AbortWithError(400, errors.New("Object not found"))
+	}
+	ctx.JSON(200, res)
+}
